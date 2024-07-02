568dda1190a507bddc3a28fce14c42e8963fbe88
kevinpham7788 <kevinpham7788@gmail.com>
2024-07-02 00:26:21 -0700

    first commit and add entities

---
+++ src/main/java/com/example/hanoi_hotel/entity/Booking.java
@@ -0,0 +1,69 @@
+package com.example.hanoi_hotel.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+
+@Data
+@Entity
+@Table(name = "Booking")
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Check In date is required")
+    private LocalDate checkInDate;
+    @Future(message = "Check out date must be in the future")
+    private LocalDate checkOutDate;
+
+    @Min(value = 1, message = "Number of adults must not be less than 1")
+    private int numOfAdults;
+
+    @Min(value = 0, message = "Number of children must no be less than 0")
+    private int numOfChildren;
+
+    private int totalNumOfGuest;
+
+    private String bookingConfirmationCode;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id", referencedColumnName = "id")
+    private Room room;
+
+    public void calculateTotalNumOfGuest() {
+        this.totalNumOfGuest = this.numOfAdults + this.numOfChildren;
+    }
+
+    public void setNumOfAdults(int numOfAdults) {
+        this.numOfAdults = numOfAdults;
+        calculateTotalNumOfGuest();
+    }
+
+    public void setNumOfChildren(int numOfChildren) {
+        this.numOfChildren = numOfChildren;
+        calculateTotalNumOfGuest();
+    }
+
+    @Override
+    public String toString() {
+        return "Booking{" +
+                "id=" + id +
+                ", checkInDate=" + checkInDate +
+                ", checkOutDate=" + checkOutDate +
+                ", numOfAdults=" + numOfAdults +
+                ", numOfChildren=" + numOfChildren +
+                ", totalNumOfGuest=" + totalNumOfGuest +
+                ", bookingConfirmationCode='" + bookingConfirmationCode + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java/com/example/hanoi_hotel/HanoiHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hanoi_hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanoiHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
