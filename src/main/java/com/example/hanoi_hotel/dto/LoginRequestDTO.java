adc1e8d32c0207d9ebca27a01731308f7304aee6
kevinpham7788 <kevinpham7788@gmail.com>
2024-07-02 10:21:32 -0700

    Add DTOs

---
+++ src/main/java/com/example/hanoi_hotel/dto/LoginRequestDTO.java
@@ -0,0 +1,13 @@
+package com.example.hanoi_hotel.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequestDTO {
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
